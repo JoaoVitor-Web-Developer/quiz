e69c2d655848015acaddd152d5fd35ea4d792999
joao <joaovitortgoliveira@gmail.com>
2025-04-01 22:20:29 -0300

    Fix: ajuste nas configurações do supabaseClient

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = "https://hwzbwszxsfqycxzafdsa.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh3emJ3c3p4c2ZxeWN4emFmZHNhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM1NTM1NTAsImV4cCI6MjA1OTEyOTU1MH0.-zDTORsxCBd7POWMd4-gBcfk3SFtuFQBEIYYSRkbYPE";
 export const supabase = createClient(supabaseUrl, supabaseKey);
